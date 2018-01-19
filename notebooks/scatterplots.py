36e0030cb737f0ca54f4836ff30dd54334d5bda3
Hao-Ting Wang <htwangtw@gmail.com>
2018-01-19 16:55:31 +0000

    emergency

--- notebooks/scatterplots.py
+++ notebooks/scatterplots.py
@@ -31,14 +31,12 @@ labels = [
 plt.close("all")
 
 sns.set_style({"font.sans-serif": ["Arial"]})
-sns.set_context('talk', font_scale=1.2)
-f, ((ax11, ax12),
-        (ax21, ax22),
-        (ax31, ax32)) = plt.subplots(
-                3, 2, figsize=(6, 8),
-                sharex='col', sharey='row')
+sns.set_context('talk', font_scale=1)
+f, ((ax11, ax12, ax13), (ax21, ax22, ax23) ) = plt.subplots(
+                2, 3, figsize=(8, 6),
+                sharey='row')
 
-axes_lst = [ax11, ax12, ax21, ax22, ax31, ax32]
+axes_lst = [ax11, ax21, ax12, ax22, ax13, ax23]
 
 for s, ax, label in zip(sig_sets, axes_lst, labels):
     x = dataset[s[0]]
@@ -46,18 +44,14 @@ for s, ax, label in zip(sig_sets, axes_lst, labels):
     X = df_X.drop(s[0], axis=1)
 
     g = sns.regplot(x, y, x_partial=X, y_partial=X,
-            marker='.', color='black', ax=ax)
+            marker='.', color='black', line_kws={"lw":1}, ax=ax)
     ax.set_ylim(-3, 3)
+
     sns.despine()
-    if label[1] == 'WIAT':
-        ax.set_xlabel(label[0])
-    else:
-        ax.set_xlabel('')
-    if label[0] == 'Component 1':
-        ax.set_ylabel('{} | Others'.format(label[1]))
-    else:
-        ax.set_ylabel(' ')
+
+    ax.set_xlabel('{}'.format(label[0]))
+    ax.set_ylabel('{}'.format(label[1]))
 f.tight_layout()
-f.subplots_adjust(top=0.92)
-f.suptitle('Adjested Variable Plots', fontsize='x-large')
+# f.subplots_adjust(top=0.92)
+# f.suptitle('Adjested Variable Plots', fontsize='x-large')
 f.savefig('./reports/plots/av-plots.png', dpi=300)

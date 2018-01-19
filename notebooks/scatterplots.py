c6481dd3fa0fc592e1a72f7a8e27e91a83ee754e
Hao-Ting Wang <htwangtw@gmail.com>
2018-01-19 14:28:08 +0000

    finalised adjusted variable plots

--- notebooks/scatterplots.py
+++ notebooks/scatterplots.py
@@ -19,21 +19,44 @@ sig_sets = [
         ('CC_03', 'WIAT_08'),
 ]
 
+labels = [
+        ('Component 1', 'Proverb'),
+        ('Component 3', 'Proverb'),
+        ('Component 1', 'WASI'),
+        ('Component 3', 'WASI'),
+        ('Component 1', 'WIAT'),
+        ('Component 3', 'WIAT')
+        ]
 
-def r_pearson(x, y):
-    return stats.pearsonr(x, y)[1]
+plt.close("all")
 
+sns.set_style({"font.sans-serif": ["Arial"]})
+f, ((ax11, ax12),
+        (ax21, ax22),
+        (ax31, ax32)) = plt.subplots(
+                3, 2, figsize=(6, 6),
+                sharex='col', sharey='row')
 
-for s in sig_sets:
+axes_lst = [ax11, ax12, ax21, ax22, ax31, ax32]
+
+for s, ax, label in zip(sig_sets, axes_lst, labels):
     x = dataset[s[0]]
     y = dataset[s[1]]
     X = df_X.drop(s[0], axis=1)
 
-    plt.close("all")#
+    g = sns.regplot(x, y, x_partial=X, y_partial=X,
+            marker='.', color='black', ax=ax)
+    ax.set_ylim(-3, 3)
     sns.despine()
-    sns.set_style({"font.sans-serif": ["Arial"]})
-    f, ax = plt.subplots(figsize=(5, 6))
-    g = sns.regplot(x, y, x_partial=X, y_partial=X, ax=ax)
-    g.set(ylim=(-3, 3))
-    sns.despine()
-    f.savefig('./reports/plots/resplots/{}_{}.png'.format(s[0], s[1]))
+    if label[1] == 'WIAT':
+        ax.set_xlabel(label[0])
+    else:
+        ax.set_xlabel('')
+    if label[0] == 'Component 1':
+        ax.set_ylabel('{} | Others'.format(label[1]))
+    else:
+        ax.set_ylabel(' ')
+f.suptitle('Adjested Variable Plots', fontsize='x-large')
+f.tight_layout()
+f.subplots_adjust(top=0.95)
+f.savefig('./reports/plots/av-plots.png')
